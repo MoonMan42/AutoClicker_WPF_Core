8cab102645a7c802953c8284d90f8206b1198aa8
MoonMan42 <miller42matt@gmail.com>
2019-12-29 17:58:18 -0500

    Added click simulation

--- AutoClicker/MainWindow.xaml.cs
+++ AutoClicker/MainWindow.xaml.cs
@@ -14,23 +14,48 @@ namespace AutoClicker
 
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
 
+        int clickCounter = 0;
+
 
         public MainWindow()
         {
             InitializeComponent();
 
             dispatcherTimer.Tick += new EventHandler(TickEvent);
-            dispatcherTimer.Interval = new TimeSpan(0,0,0,0, Int32.Parse(TimeBox.Text));        
+            
+
             
         }
 
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
         private void TickEvent(object sender, EventArgs e)
         {
-            Console.Beep();
+            LeftMouseClick(0, 0);
+            clickCounter += 1;
+            clickCountLabel.Content = clickCounter;
+        }
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            //SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
         }
 
         private void StartBtn_Clicked(object sender, RoutedEventArgs e)
         {
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, Int32.Parse(TimeBox.Text));
             dispatcherTimer.Start();
 
         }
@@ -44,6 +69,12 @@ namespace AutoClicker
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            clickCounter = 0;
+            clickCountLabel.Content = clickCounter;
         }
     }
 }
